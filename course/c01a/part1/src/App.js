06409197d1dd27f01d59d7e0e41e6bf1baca4e2c
maju84 <max-j@gmx.de>
2022-04-15 19:02:59 +0200

    having finished course part 01c

--- course/c01a/part1/src/App.js
+++ course/c01a/part1/src/App.js
@@ -1,21 +1,32 @@
-const Hello = (props) => { 
-  return (
-    <div>      
-      <p>Hello {props.name} your age is {props.age}</p>    
-    </div>
-  ); 
-};
+import { useState } from 'react'
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => (
+    <button onClick={ onClick }>
+      {text}
+    </button>
+  )
+
+
 
 const App = () => {
-  const age = 42;
-  console.log('Hello from component.');
+  // useState hook to create and init app state 
+  const [ counter, setCounter ] = useState(0)
+
+  // event handler methods
+  const incrementCounter = () => setCounter(counter + 1)
+  const decreaseCounter = () => setCounter(counter -1)
+  const resetCounter = () => setCounter(0)
+
   return (
     <div>
-      <p>Greetings now at {new Date().toString()}</p>
-      <Hello name="Dude" age="23"/>
-      <Hello name="Dave" age={age}/>
-      <Hello />
-    </div>);
-};
+      <Display counter={counter}/>
+      <Button onClick={ incrementCounter } text='+1'/>
+      <Button onClick={ decreaseCounter } text='-1'/>
+      <Button onClick={ resetCounter } text=' 0 '/>     
+    </div>
+  )
+}
 
-export default App;
+export default App
