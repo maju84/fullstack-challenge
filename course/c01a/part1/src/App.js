656e567b56e159160025f1a792ce6f193c41fc86
maju84 <max-j@gmx.de>
2022-04-15 19:55:06 +0200

    finish cource 01d-higher order functions

--- course/c01a/part1/src/App.js
+++ course/c01a/part1/src/App.js
@@ -22,27 +22,19 @@ const Button = ({ handleClick, text }) => (
 )
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const [value, setValue] = useState(10)
 
-  const handleLeftClick = () => {    
-    setAll(allClicks.concat('L'))     // concat returns a copy. DO NOT allClicks.push('L')
-    setLeft(left + 1)  
-  }
 
-  const handleRightClick = () => {    
-    setAll(allClicks.concat('R'))    
-    setRight(right + 1)  
+  const setToValue = (newValue) => () => {    
+    console.log('value now', newValue)  // print the new value to console    
+    setValue(newValue)  
   }
 
   return (
     <div>
-      {left}
-        <Button handleClick={handleLeftClick} text='left' />
-        <Button handleClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />   
+      {value}
+      <Button handleClick={setToValue(0)} text='reset' />
+      <Button handleClick={setToValue(value+1)} text=' + 1' />
     </div>
   )
 }
