b5bf805afb6283dee9042a0106c3c24c0cffe36a
maju84 <max-j@gmx.de>
2022-04-15 21:37:51 +0200

    exercise 1.8

--- exercise/p01/unicafe/src/App.js
+++ exercise/p01/unicafe/src/App.js
@@ -9,62 +9,58 @@ const Button = ({ handleClick, text }) => (
 const Display = ({ text, value, unit='' }) => 
   <div>{text}: {value} {unit}</div> 
 
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good+neutral+bad;
+  const average = (good-bad) / all;
+  
+  return (
+    <>
+    <h1>statistics</h1>
+    { all 
+      ? 
+      <>
+      <Display text='good' value={good} />
+      <Display text='neutral' value={neutral} />
+      <Display text='bad' value={bad} />
+      <Display text='all' value={all} />
+      <Display text='average' value={average} />
+      <Display text='positive' value={good*100 / all } unit='%' />
+      </> 
+
+      : null
+    }
+    </>
+  )
+}
+ 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
 
-
-
-  const clickedGood = () => {
-    setGood(good+1)
-    const newAll = all+1;
-    setAll(newAll)
-    setAverage( (good+1-bad) / newAll )
-  }
-
-  const clickedNeutral = () => {
-    setNeutral(neutral+1)
-    const newAll = all+1;
-    setAll(newAll)
-    setAverage( (good-bad) / newAll )
-  }
-
-  const clickedBad = () => {
-    setBad(bad+1)
-    const newAll = all+1;
-    setAll(newAll)
-    setAverage( (good-bad-1) / newAll )
-  }
-
- 
 
   return (
     <div>
       <h1>give feedback</h1>
       <Button 
-        handleClick={clickedGood}
+        handleClick={() => setGood(good+1)}
         text='good'
       />
       <Button 
-        handleClick={clickedNeutral}
+        handleClick={() => setNeutral(neutral+1)}
         text='neutral'
       />
       <Button 
-        handleClick={clickedBad}
+        handleClick={() => setBad(bad+1)}
         text='bad'
       />
-
-      <h1>statistics</h1>
-      <Display text='good' value={good} />
-      <Display text='neutral' value={neutral} />
-      <Display text='bad' value={bad} />
-      <Display text='all' value={good+neutral+bad} />
-      <Display text='average' value={average} />
-      <Display text='positive' value={good*100 / (all?all:1) } unit='%' />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+      />      
     </div>
   )
 }
