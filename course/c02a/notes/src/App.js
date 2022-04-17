da4a53fa8816e92872405b4db2a9e70fe066f5ad
maju84 <max-j@gmx.de>
2022-04-17 16:21:05 +0200

    finish course 02b - filter displayed elements

--- course/c02a/notes/src/App.js
+++ course/c02a/notes/src/App.js
@@ -3,9 +3,8 @@ import Note from './components/Note'
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState(    
-    'a new note...'  
-  ) 
+  const [newNote, setNewNote] = useState( 'a new note...' )
+  const [showAll, setShowAll] = useState(true)
 
   const addNote = (event) => {    
     event.preventDefault()    
@@ -28,11 +27,21 @@ const App = (props) => {
   }
 
 
+  const notesToShow = showAll    
+    ? notes    
+    : notes.filter(note => note.important)
+
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>        
+        <button onClick={() => setShowAll(!showAll)}>          
+          show {showAll ? 'important' : 'all' }        
+        </button>      
+      </div>
       <ul>
-        {notes.map( note => 
+        {notesToShow.map( note => 
           <Note 
             key={note.id} 
             content={note.content} 
