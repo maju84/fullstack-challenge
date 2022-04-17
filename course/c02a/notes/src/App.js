1741a1a4f8fb9a40394d62e2f15e1d154cc4797e
maju84 <max-j@gmx.de>
2022-04-17 15:05:03 +0200

    finish cource 02a-refactoring modules

---
+++ course/c02a/notes/src/App.js
@@ -0,0 +1,20 @@
+import Note from './components/Note'
+
+const App = ({ notes }) => {
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map( note => 
+          <Note 
+            key={note.id} 
+            content={note.content} 
+          />)
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default App
