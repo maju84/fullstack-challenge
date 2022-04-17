31ffc9deb47b0343e0d8a4129fc4a80f461b2687
maju84 <max-j@gmx.de>
2022-04-17 17:44:12 +0200

    exercise 02.9 - phonebook step4

--- exercise/p02/phonebook/src/App.js
+++ exercise/p02/phonebook/src/App.js
@@ -3,13 +3,15 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { id: 1,
-      name: 'Arto Hellas', 
-      number: '123-42',
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  const [newSearchString, setNewSearchString] = useState('')
   
 
   const addPerson = (event) => {    
@@ -56,9 +58,34 @@ const App = () => {
     setNewNumber(event.target.value)  
   }
 
+  const handleSearchStringChange = (event) => {  
+    setNewSearchString(event.target.value)  
+  }
+
+  const filterPersons = ({ searchString }) => {
+    const search = searchString.trim();
+    if (search.length === 0) {
+      return persons;
+    }
+
+    return persons.filter(person => 
+      person.name.toLowerCase().includes(search))
+  }
+
+  const personsDisplayed = filterPersons({ searchString: newSearchString })
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown containing: 
+          <input 
+            value={newSearchString}
+            onChange={handleSearchStringChange}
+          />
+        </div>
+
+      <h2>add new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -76,8 +103,9 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
-        { persons.map((person) => 
+        { personsDisplayed.map((person) => 
           <Person 
             key={person.id} 
             person={person} 
